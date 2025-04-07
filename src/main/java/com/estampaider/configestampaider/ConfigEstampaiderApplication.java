fa61fd4b336d08d7c10aa0a0b03e46428db42764
Jaider Correa <jaico98@hotmail.com>
2025-04-07 13:24:06 -0500

    Primer commit - configuración del servidor

---
+++ src/main/java/com/estampaider/configestampaider/ConfigEstampaiderApplication.java
@@ -0,0 +1,13 @@
+package com.estampaider.configestampaider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigEstampaiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigEstampaiderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estampaider/configestampaider/ConfigEstampaiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estampaider.configestampaider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigEstampaiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
